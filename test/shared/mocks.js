25c7d852897cafb4756de46bcdbe9530109fd458
yan-man <yan.man45@gmail.com>
2022-05-18 15:07:39 -0500

    remove ERC-20 token;

--- test/shared/mocks.js
+++ test/shared/mocks.js
@@ -1,16 +1,5 @@
 const { waffle } = require("hardhat");
-const ERC_20_ABI = require("../../abis/erc20.abi.json");
 const AppraiserOrganization_ABI = require("../../artifacts/contracts/AppraiserOrganization.sol/AppraiserOrganization.json");
-
-async function deployMockREPToken(deployer) {
-  const erc20 = await waffle.deployMockContract(deployer, ERC_20_ABI);
-
-  await erc20.mock.name.returns(`Reputation Coin`);
-  await erc20.mock.symbol.returns(`REP`);
-  await erc20.mock.transferFrom.returns(true);
-
-  return erc20;
-}
 
 async function deployMockAppraiserOrganization(deployer) {
   const appraiserOrganization = await waffle.deployMockContract(
@@ -21,4 +10,4 @@ async function deployMockAppraiserOrganization(deployer) {
   return appraiserOrganization;
 }
 
-module.exports = { deployMockREPToken, deployMockAppraiserOrganization };
+module.exports = { deployMockAppraiserOrganization };
