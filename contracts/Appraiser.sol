17fc301ab60913c0cf2377d82ddfd37becf82c6a
yan-man <yan.man45@gmail.com>
2022-05-20 20:08:28 -0500

    split contracts into separate smaller contracts - Appraiser, Reviewer;

--- contracts/Appraiser.sol
+++ contracts/Appraiser.sol
@@ -8,40 +8,30 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "./AppraiserOrganization.sol";
 import "./Verifier.sol";
 import "./Users.sol"; // to use struct
+import "./Reviewer.sol"; // to use struct
 
 contract Appraiser is Ownable {
     using Counters for Counters.Counter;
-    using Users for Users.User;
 
     // State Vars
     Counters.Counter public s_orgIds;
     mapping(uint256 => uint256) public s_organizations; // orgId -> intbool isActive
     mapping(uint256 => address) public s_vContracts; // orgId -> deployed Verifier contract
-    mapping(uint256 => address) public s_aoContracts; // orgId -> deployed AO contract
     mapping(string => uint256) private s_orgNames; // org name -> intbool exists flag
+    address private s_reviewerContract;
     mapping(uint256 => mapping(uint256 => address)) public s_reviews; // orgId -> reviewId -> reviewer address
-    mapping(address => Users.User) public s_users; // user/reviewer address -> User struct
 
     // Events
     event LogAddOrganization(uint256 orgId);
-    event LogNFTContractDeployed(address aoContractAddress);
-    event LogMintReview(uint256 reviewId);
-    event LogNewUser(address addr);
-    event LogVoteOnReview(address voter, uint256 orgId, uint256 reviewId);
     event LogVerifierNFTContractDeployed(address verifierContractAddress);
 
     // Errors
     error Appraiser__DuplicateOrgName();
-    error Appraiser__InvalidOrgId();
-    error Appraiser__VoterMatchesAuthor();
-    error Appraiser__InvalidReview();
 
     // Modifiers
-    modifier isValidOrgId(uint256 orgId_) {
-        if (address(s_aoContracts[orgId_]) == address(0)) {
-            revert Appraiser__InvalidOrgId();
-        }
-        _;
+
+    constructor(address reviewerContract_) {
+        s_reviewerContract = reviewerContract_;
     }
 
     function addOrganization(
@@ -101,71 +91,14 @@ contract Appraiser is Ownable {
             URI_,
             verifierAddr_
         );
-        s_aoContracts[orgId_] = address(_ao);
-        emit LogNFTContractDeployed(address(_ao));
-    }
 
-    function setAOContractAddress(uint256 orgId_, address aoAddress_)
-        public
-        onlyOwner
-        isValidOrgId(orgId_)
-    {
-        s_aoContracts[orgId_] = aoAddress_;
-    }
-
-    function mintReview(
-        uint256 orgId_,
-        uint256 rating_,
-        string calldata review_
-    ) external isValidOrgId(orgId_) {
-        uint256 _reviewId = AppraiserOrganization(s_aoContracts[orgId_])
-            .mintReviewNFT(msg.sender, rating_, review_);
-        s_reviews[orgId_][_reviewId] = msg.sender;
-        addUser(msg.sender);
-        emit LogMintReview(_reviewId);
-    }
-
-    function addUser(address addr_) private {
-        if (s_users[addr_].isRegistered == false) {
-            s_users[addr_] = Users.User({
-                upvotes: 0,
-                downvotes: 0,
-                isRegistered: true
-            });
-
-            emit LogNewUser(addr_);
-        }
-    }
-
-    function voteOnReview(
-        uint256 orgId_,
-        uint256 reviewId_,
-        bool isUpvote_
-    ) external isValidOrgId(orgId_) {
-        address _reviewAuthorAddr = s_reviews[orgId_][reviewId_];
-        if (_reviewAuthorAddr == address(0)) {
-            revert Appraiser__InvalidReview();
-        }
-        if (msg.sender == _reviewAuthorAddr) {
-            revert Appraiser__VoterMatchesAuthor();
-        }
-
-        Users.User storage _reviewUser = s_users[s_reviews[orgId_][reviewId_]];
-        if (isUpvote_ == true) {
-            _reviewUser.upvotes += 1;
-        } else {
-            _reviewUser.downvotes += 1;
-        }
-        AppraiserOrganization(s_aoContracts[orgId_]).voteOnReview(
-            msg.sender,
-            reviewId_,
-            isUpvote_
+        Reviewer(s_reviewerContract).setApprovalOrganizationContractAddress(
+            orgId_,
+            address(_ao)
         );
-
-        emit LogVoteOnReview(msg.sender, orgId_, reviewId_);
     }
 
-    function numberOrganizations() public view returns (uint256) {
+    function numberOrganizations() external view returns (uint256) {
         return s_orgIds.current();
     }
 }
---
+++ contracts/Reviewer.sol
@@ -0,0 +1,107 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "./Reviews.sol";
+import "./AppraiserOrganization.sol";
+import "./Users.sol";
+
+contract Reviewer is Ownable {
+    using Counters for Counters.Counter;
+    using Reviews for Reviews.Review;
+    using Users for Users.User;
+
+    // state vars
+    mapping(uint256 => Reviews.Review) public s_verifiers; // orgId -> # of tokens
+    address public s_appraiserContract;
+    mapping(uint256 => address) public s_aoContracts; // orgId -> deployed AO contract
+    mapping(uint256 => mapping(uint256 => address)) public s_reviews; // orgId -> reviewId -> reviewer address
+    mapping(address => Users.User) public s_users; // user/reviewer address -> User struct
+
+    // events
+    event LogMintReview(uint256 reviewId);
+    event LogNewUser(address addr);
+    event LogVoteOnReview(address voter, uint256 orgId, uint256 reviewId);
+
+    // errors
+    error Appraiser__InvalidOrgId();
+    error Appraiser__VoterMatchesAuthor();
+    error Appraiser__InvalidReview();
+
+    // modifiers
+    modifier isValidOrgId(uint256 orgId_) {
+        _isValidOrgId(orgId_);
+        _;
+    }
+
+    function _isValidOrgId(uint256 orgId_) private view {
+        if (address(s_aoContracts[orgId_]) == address(0)) {
+            revert Appraiser__InvalidOrgId();
+        }
+    }
+
+    constructor() {}
+
+    function addUser(address addr_) private {
+        if (s_users[addr_].isRegistered == false) {
+            s_users[addr_] = Users.User({
+                upvotes: 0,
+                downvotes: 0,
+                isRegistered: true
+            });
+
+            emit LogNewUser(addr_);
+        }
+    }
+
+    function mintReview(
+        uint256 orgId_,
+        uint256 rating_,
+        string calldata review_
+    ) external isValidOrgId(orgId_) {
+        uint256 _reviewId = AppraiserOrganization(s_aoContracts[orgId_])
+            .mintReviewNFT(msg.sender, rating_, review_);
+        s_reviews[orgId_][_reviewId] = msg.sender;
+        addUser(msg.sender);
+        emit LogMintReview(_reviewId);
+    }
+
+    function setApprovalOrganizationContractAddress(
+        uint256 orgId_,
+        address contractAddr_
+    ) external {
+        s_aoContracts[orgId_] = contractAddr_;
+    }
+
+    function voteOnReview(
+        uint256 orgId_,
+        uint256 reviewId_,
+        bool isUpvote_
+    ) external isValidOrgId(orgId_) {
+        address _reviewAuthorAddr = s_reviews[orgId_][reviewId_];
+        if (_reviewAuthorAddr == address(0)) {
+            revert Appraiser__InvalidReview();
+        }
+        if (msg.sender == _reviewAuthorAddr) {
+            revert Appraiser__VoterMatchesAuthor();
+        }
+
+        Users.User storage _reviewUser = s_users[s_reviews[orgId_][reviewId_]];
+        if (isUpvote_ == true) {
+            _reviewUser.upvotes += 1;
+        } else {
+            _reviewUser.downvotes += 1;
+        }
+        AppraiserOrganization(s_aoContracts[orgId_]).voteOnReview(
+            msg.sender,
+            reviewId_,
+            isUpvote_
+        );
+
+        emit LogVoteOnReview(msg.sender, orgId_, reviewId_);
+    }
+}
