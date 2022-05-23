e8784dedcd42f25206c5a9a1b3919ba0529374cc
yan-man <yan.man45@gmail.com>
2022-05-23 14:47:51 -0500

    initialize front end with Home/Player from Netflix template;

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from "web3uikit";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider appId="xxxxx" serverUrl="xxxxx">
+      <NotificationProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </NotificationProvider>
+    </MoralisProvider>
   </React.StrictMode>
 );
 
